36f8257faea4ae964ef8542f8d7e4154e4e01a30
Muhamad Raafi <freecukup@gmail.com>
2018-07-29 14:52:47 +0700

    new db

--- 10115104_aplikasitokoonline.sql
+++ 10115104_aplikasitokoonline.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 29 Jul 2018 pada 09.46
+-- Generation Time: 29 Jul 2018 pada 09.52
 -- Versi Server: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -48,6 +48,52 @@ INSERT INTO `akun` (`id`, `username`, `password`, `nama`, `alamat`, `no_telp`, `
 (4, 'abdull', 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4', 'badull', 'jl jkt ', '9021894729479', 'pelanggan', '4a6451b2534bce654b899fb1a'),
 (5, 'secret', 'e5e9fa1ba31ecd1ae84f75caaa474f3a663f05f4', 'james', 'secret', '0921876', 'pelanggan', '002gotosleepforever_465_5');
 
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` smallint(6) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `harga` int(6) NOT NULL,
+  `stok` tinyint(4) NOT NULL,
+  `kategori` enum('kaos','long sleeve','kemeja','') NOT NULL,
+  `foto` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `keranjang`
+--
+
+CREATE TABLE `keranjang` (
+  `id` smallint(6) NOT NULL,
+  `tgl_ditambah` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_akun` smallint(6) NOT NULL,
+  `id_barang` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` smallint(6) NOT NULL,
+  `tgl_beli` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `tgl_dikirim` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `jumlah` tinyint(4) NOT NULL,
+  `total_harga` int(9) NOT NULL,
+  `foto_bukti` varchar(25) NOT NULL,
+  `status_transaksi` enum('belum dikirim','sudah dikirim') NOT NULL,
+  `id_akun` smallint(6) NOT NULL,
+  `id_barang` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -59,6 +105,28 @@ ALTER TABLE `akun`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_akun` (`id_akun`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_akun` (`id_akun`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -67,6 +135,39 @@ ALTER TABLE `akun`
 --
 ALTER TABLE `akun`
   MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `keranjang`
+--
+ALTER TABLE `keranjang`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `keranjang`
+--
+ALTER TABLE `keranjang`
+  ADD CONSTRAINT `keranjang_ibfk_1` FOREIGN KEY (`id_akun`) REFERENCES `akun` (`id`),
+  ADD CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_akun`) REFERENCES `akun` (`id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
